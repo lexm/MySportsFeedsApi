653a43025388d3cf1cb9bc7be31c928435c6d988
Alexander J Woods <alexanderjwoods@vivaldi.net>
2017-08-27 23:40:01 -0400

    Added sample project

---
+++ SampleProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using MySportsFeeds;
+
+using System;
+
+namespace MySportsFeedsAPIExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MySportsFeedsApi.Username = "YOURUSERNAME";
+            MySportsFeedsApi.Password = "YOURPASSWORD";
+
+            Console.WriteLine(MySportsFeedsApi.FetchFeedAsync("mlb", "2016-regular", "division_team_standings").Result);
+            Console.ReadLine();
+        }
+    }
+}
